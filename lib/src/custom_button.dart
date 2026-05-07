bca3e2e13a4b254c9bb40077be36a1392bef7c17
KicaiBadai <adityamaula80@gmail.com>
2026-05-07 11:06:57 +0700

    isi custom button

---
+++ lib/src/custom_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+  final Color color;
+
+  const CustomButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+    this.color = Colors.blue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
